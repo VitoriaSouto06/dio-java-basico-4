2c64d97bc118c1f79d5d850ddea910124299463d
VitoriaSouto06 <soutovitoria06@gmail.com>
2025-02-18 12:56:11 -0300

    Implementaçao das aulas

---
+++ dio-java-banco/src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+       Conta c1 = new ContaCorrente();
+       Conta c2 = new ContaPoupanca();
+
+       c1.imprimirExtrato();
+        
+    }           
+}
